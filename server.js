9c64a4a6680bf020f05f6e482d9eecc0f867144b
mukeshchacha45-max <mukeshchacha45@gmail.com>
2026-09-24 16:49:58 +0530

    Update server.js

--- server.js
+++ server.js
@@ -804,11 +804,8 @@ app.get("/", (req, res) => {
   });
 });
 
-server.listen(
-  5000,
-  () => {
-    console.log(
-      "Server running on http://localhost:5000"
-    );
-  }
-);
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
